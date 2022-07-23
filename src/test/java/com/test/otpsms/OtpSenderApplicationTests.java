5764b4672348d0248aed8a50b4f4ff5d0d0678e4
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-23 09:11:58 +0530

    Initial commit

---
+++ src/test/java/com/test/otpsms/OtpSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.otpsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
